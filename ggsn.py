64d710bf1519d2e2079d7744e9aeb3ddd6a7db5e
Florian Metzger <florian.metzger@univie.ac.at>
2013-09-11 10:19:46 +0200

    append startupTime to results filename

--- ggsn.py
+++ ggsn.py
@@ -78,7 +78,7 @@ class Multiserver_GGSN(Base_GGSN):
     def __init__(self, env, options):
         Base_GGSN.__init__(self, env, options)
         self.hv = Hypervisor(self, env, options)
-        self.resultsbasename = "_{0}_{1}_{2}.csv".format(self.numberOfSupportedParallelTunnels, self.hv.numberOfMaxInstances, options.duration)
+        self.resultsbasename = "_{0}_{1}_{2}_{3}.csv".format(self.numberOfSupportedParallelTunnels, self.hv.numberOfMaxInstances, options.startupTime, options.duration)
 
 
 
